d8ec71e57714bb2b2cbd698f1b3141737c14ff3f
videx <45696129+odejinmi@users.noreply.github.com>
2026-01-28 18:23:04 +0100

    social login added

--- lib/app/modules/login_screen_module/login_screen_page.dart
+++ lib/app/modules/login_screen_module/login_screen_page.dart
@@ -1,3 +1,7 @@
+import 'dart:developer' as dev;
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:mcd/core/import/imports.dart';
 // import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
@@ -316,88 +320,89 @@ class LoginScreenPage extends GetView<LoginScreenController> {
                                 ),
                               ),
                             )),
-                        // const Gap(20),
+                        const Gap(20),
 
-                        // Center(child: TextSemiBold("Or")),
-                        // const Gap(20),
+                        Center(child: TextSemiBold("Or")),
+                        const Gap(20),
 
-                        // Row(
-                        //   mainAxisAlignment: MainAxisAlignment.center,
-                        //   children: [
-                        //     // Facebook login
-                        //     InkWell(
-                        //       onTap: () async {
-                        //         try {
-                        //           final LoginResult fbResult = await FacebookAuth.instance.login(
-                        //             permissions: ['email', 'public_profile'],
-                        //           );
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // Facebook login
+                            InkWell(
+                              onTap: () async {
+                                try {
+                                  final LoginResult fbResult = await FacebookAuth.instance.login(
+                                    permissions: ['email', 'public_profile'],
+                                  );
 
-                        //           if (fbResult.status == LoginStatus.success) {
-                        //             final userData = await FacebookAuth.instance.getUserData();
+                                  if (fbResult.status == LoginStatus.success) {
+                                    final userData = await FacebookAuth.instance.getUserData();
 
-                        //             final email = userData['email'] ?? '';
-                        //             final name = userData['name'] ?? '';
-                        //             final avatar = userData['picture']?['data']?['url'] ?? '';
-                        //             final accessToken = fbResult.accessToken!.tokenString;
-                        //             // Sign in to Firebase with the Facebook credential to keep auth in sync
-                        //             final credential = FacebookAuthProvider.credential(accessToken);
-                        //             final firebaseUser = await FirebaseAuth.instance.signInWithCredential(credential);
-                        //             final firebaseIdToken = await firebaseUser.user?.getIdToken();
-                        //             const source = 'facebook';
+                                    final email = userData['email'] ?? '';
+                                    final name = userData['name'] ?? '';
+                                    final avatar = userData['picture']?['data']?['url'] ?? '';
+                                    final accessToken = fbResult.accessToken!.tokenString;
+                                    // Sign in to Firebase with the Facebook credential to keep auth in sync
+                                    final credential = FacebookAuthProvider.credential(accessToken);
+                                    final firebaseUser = await FirebaseAuth.instance.signInWithCredential(credential);
+                                    final firebaseIdToken = await firebaseUser.user?.getIdToken();
+                                    const source = 'facebook';
 
-                        //             await controller.socialLogin(
-                        //               context,
-                        //               email,
-                        //               name,
-                        //               avatar,
-                        //               accessToken,
-                        //               source,
-                        //               firebaseIdToken: firebaseIdToken,
-                        //             );
-                        //             dev.log('Facebook login successful');
-                        //           } else if (fbResult.status == LoginStatus.cancelled) {
-                        //             Get.snackbar(
-                        //               "Login Cancelled",
-                        //               "Facebook login was cancelled",
-                        //               backgroundColor: AppColors.errorBgColor,
-                        //               colorText: AppColors.textSnackbarColor,
-                        //             );
-                        //           } else {
-                        //             Get.snackbar(
-                        //               "Error",
-                        //               "Facebook login failed: ${fbResult.message}",
-                        //               backgroundColor: AppColors.errorBgColor,
-                        //               colorText: AppColors.textSnackbarColor,
-                        //             );
-                        //           }
-                        //         } catch (e) {
-                        //           dev.log("Facebook login error: $e");
-                        //           Get.snackbar(
-                        //             "Error",
-                        //             "Facebook login error: $e",
-                        //             backgroundColor: AppColors.errorBgColor,
-                        //             colorText: AppColors.textSnackbarColor,
-                        //           );
-                        //         }
-                        //       },
-                        //       child: SvgPicture.asset(AppAsset.facebook, width: 50),
-                        //     ),
-                        //     const Gap(10),
-                        //     // Google login
-                        //     // TODO: Google Sign-In v7.x has different API - needs platform-specific implementation
-                        //     InkWell(
-                        //       onTap: () async {
-                        //         Get.snackbar(
-                        //           "Coming Soon",
-                        //           "Google Sign-In will be available soon",
-                        //           backgroundColor: AppColors.errorBgColor,
-                        //           colorText: AppColors.textSnackbarColor,
-                        //         );
-                        //       },
-                        //       child: Image.asset(AppAsset.google, width: 50),
-                        //     ),
-                        //   ],
-                        // ),
+                                    await controller.socialLogin(
+                                      context,
+                                      email,
+                                      name,
+                                      avatar,
+                                      accessToken,
+                                      source,
+                                      firebaseIdToken: firebaseIdToken,
+                                    );
+                                    dev.log('Facebook login successful');
+                                  } else if (fbResult.status == LoginStatus.cancelled) {
+                                    Get.snackbar(
+                                      "Login Cancelled",
+                                      "Facebook login was cancelled",
+                                      backgroundColor: AppColors.errorBgColor,
+                                      colorText: AppColors.textSnackbarColor,
+                                    );
+                                  } else {
+                                    Get.snackbar(
+                                      "Error",
+                                      "Facebook login failed: ${fbResult.message}",
+                                      backgroundColor: AppColors.errorBgColor,
+                                      colorText: AppColors.textSnackbarColor,
+                                    );
+                                  }
+                                } catch (e) {
+                                  dev.log("Facebook login error: $e");
+                                  Get.snackbar(
+                                    "Error",
+                                    "Facebook login error: $e",
+                                    backgroundColor: AppColors.errorBgColor,
+                                    colorText: AppColors.textSnackbarColor,
+                                  );
+                                }
+                              },
+                              child: SvgPicture.asset(AppAsset.facebook, width: 50),
+                            ),
+                            const Gap(10),
+                            // Google login
+                            // TODO: Google Sign-In v7.x has different API - needs platform-specific implementation
+                            InkWell(
+                              onTap: () async {
+                                controller.handleSignIn(context);
+                                // Get.snackbar(
+                                //   "Coming Soon",
+                                //   "Google Sign-In will be available soon",
+                                //   backgroundColor: AppColors.errorBgColor,
+                                //   colorText: AppColors.textSnackbarColor,
+                                // );
+                              },
+                              child: Image.asset(AppAsset.google, width: 50),
+                            ),
+                          ],
+                        ),
                         // const Expanded(child: SetFingerPrint()),
 
                         const Gap(30),
